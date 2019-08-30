3a1395657417f3e6ad45918d8e27a871435c625a
Yuri Brunetto <brunettoyuri@gmail.com>
2019-08-30 11:37:41 -0300

    33 - restructure post item

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import * as S from './styled'
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug/">
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>30 de Agosto de 2019 - 4 min de leitura</S.PostItemDate>
-        <S.PostItemTitle>
-          Diga não ao Medium: tenha sua própria plataforma
-        </S.PostItemTitle>
-        <S.PostItemDescription>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos,
-          nisi dolorum! Odio, facilis minima. Fugit aliquam sapiente similique
-          deleniti. Non?
-        </S.PostItemDescription>
+        <S.PostItemDate>
+          {date} &bull; {timeToRead} min de leitura
+        </S.PostItemDate>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem
