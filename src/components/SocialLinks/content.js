61bdce1db68dfe91b4e4d1a814bc8ac045da0a35
Yuri Brunetto <brunettoyuri@gmail.com>
2019-08-30 10:43:20 -0300

    29 - social links

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/YuriBrunetto',
+  },
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/yuribrunetto`,
+  },
+  {
+    label: 'Youtube',
+    url: `https://www.youtube.com/yuribrunetto`,
+  },
+  {
+    label: 'Instagram',
+    url: `https://www.instagram.com/ybrntt`,
+  },
+  {
+    label: 'Unsplash',
+    url: `https://unsplash.com/yuribrunetto`,
+  },
+]
+
+export default links
