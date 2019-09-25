8c10601a21f0d5b65d071b54f08248ddf8016a40
Yuri Brunetto <brunettoyuri@gmail.com>
2019-09-25 13:29:49 -0300

    74 - getThemeColor

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Avatar from '../Avatar'
+import getThemeColor from '../../utils/getThemeColor'
+
 import * as S from './styled'
+
 
 const Profile = () => {
   const {
@@ -23,7 +26,7 @@ const Profile = () => {
 
   return (
     <S.ProfileWrapper>
-      <S.ProfileLink to="/" cover direction="left" bg="#16202c" duration={0.6}>
+      <S.ProfileLink to="/" cover direction="left" bg={getThemeColor()} duration={0.6}>
         <Avatar />
         <S.ProfileAuthor>
           {title}
